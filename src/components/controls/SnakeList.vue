979c977c1d5d4b1ef8d844028c489bde8ae578d7
ttl <talrasha007@gmail.com>
2018-02-11 17:11:17 +0800

    list style.

--- src/components/controls/SnakeList.vue
+++ src/components/controls/SnakeList.vue
@@ -3,21 +3,36 @@
     <li v-for="(snake, idx) in snakes" :key="snake.id">
       <router-link :to="{ path: '' }">
         <div class="snake-detail" :class="{ ['s' + idx % 5]: true }">
+          <div v-if="snake.currentPrice >= 0" class="snake-status">
+            <font-awesome-icon class="icon" :icon="['fab', 'ethereum']" />
+            <span class="price">&nbsp;{{snake.currentPrice | wei}}</span>
+          </div>
 
         </div>
         <div>
           <span class="snake-id">#{{snake.id}}</span>
         </div>
       </router-link>
-      <!--{{snake}}-->
     </li>
   </ul>
 </template>
 
 <script>
+import web3 from '../../lib/web3';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+
+
 export default {
   name: 'snake-list',
-  props: ['snakes']
+  props: ['snakes'],
+  filters: {
+    wei(v) {
+      return v >= 0 && Math.floor(web3.fromWei(v) * Math.pow(10, 5)) / Math.pow(10, 5);
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  }
 }
 </script>
 
@@ -40,10 +55,26 @@ ul {
       text-decoration: none;
 
       .snake-detail {
+        position: relative;
         width: 280px;
         height: 280px;
         margin: 10px;
         border-radius: 5px;
+
+        .snake-status {
+          border-radius: 10px;
+          background-color: rgba(255, 255, 255, 0.5);
+          position: absolute;
+          top: 16px;
+          left: 50%;
+          padding: 5px 15px;
+          transform: translate(-50%, 0);
+          color: #0b0b0b;
+
+          .price {
+            color: #7c7c7b;
+          }
+        }
 
         &.s0 {
           background-color: #d1eeeb;
