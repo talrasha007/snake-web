c959a0f2bf8e22678030f5ef95c79a169ade9a9f
Tal Rasha <talrasha007@gmail.com>
2018-02-12 23:15:16 +0800

    snake item.

---
+++ src/components/controls/SnakeDesc.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="snake-container">
+    <div class="snake-detail" :class="{ ['s' + snake.id % 8]: true }">
+      <div v-if="snake.sale" class="snake-status">
+        <font-awesome-icon class="icon" :icon="['fab', 'ethereum']" />
+        <span class="price">&nbsp;{{snake.sale.currentPrice | wei}}</span>
+      </div>
+      <div v-if="snake.siring" class="snake-status">
+        <font-awesome-icon class="icon" :icon="['fas', 'transgender-alt']" />
+        <span class="price">&nbsp;{{snake.siring.currentPrice | wei}}</span>
+      </div>
+      <div class="genes">{{snake.genes | genes}}</div>
+    </div>
+    <div class="snake-meta">
+      <span class="snake-id">#{{snake.id}}</span>
+      <span>{{snake.generation}}代</span>
+      <span>{{snake.cooldownIndex | cooldown}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import { wei, cooldown, genes } from '../filters';
+
+export default {
+  name: 'snake-desc',
+
+  props: ['snake'],
+
+  filters: {
+    wei, cooldown, genes
+  },
+
+  components: {
+    FontAwesomeIcon
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$large-size: 280px;
+$md-size: 200px;
+$sm-size: 140px;
+
+.snake-container {
+  width: $large-size;
+  margin: 0 auto;
+
+  &.md {
+    width: $md-size;
+
+    .snake-detail {
+      width: $md-size;
+      height: $md-size;
+    }
+  }
+
+  &.sm {
+    width: $sm-size;
+
+    .snake-detail {
+      width: $sm-size;
+      height: $sm-size;
+    }
+
+    .genes {
+      display: none;
+    }
+  }
+
+  .snake-detail {
+    position: relative;
+    width: $large-size;
+    height: $large-size;
+    margin: 10px 0;
+    border-radius: 5px;
+
+    .snake-status {
+      border-radius: 10px;
+      background-color: rgba(255, 255, 255, 0.5);
+      position: absolute;
+      top: 16px;
+      left: 50%;
+      padding: 5px 15px;
+      transform: translate(-50%, 0);
+      color: #0b0b0b;
+
+      .price {
+        color: #82817d;
+      }
+    }
+
+    .genes {
+      position: absolute;
+      top: 50%;
+      transform: translate(0, -40%);
+      color: #82817d;
+      padding: 0 40px;
+      word-break: break-all;
+    }
+  }
+
+  .snake-meta {
+    color: #82817d;
+
+    .snake-id {
+      color: #2a2825;
+    }
+  }
+}
+</style>
